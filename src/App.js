a2b45a9ab0931660940be54cabf88d80108afb46
ozantalay <ozntalay@hotmail.com>
2024-10-02 00:32:14 +0300

    baslangic

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+function App() {
+  return <Pagination />;
+}
+
+const Pagination = () => {
+  // KODUNUZ BURAYA EKLENECEK
+};
+
+const Pages = ({ content, itemsPerPage }) => {};
+
+export default App;
